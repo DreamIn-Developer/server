ccb48707e67e1ec1fafebfa28671cff6f4178d6f
leeceo97 <leeceo97@naver.com>
2022-04-30 15:55:46 +0900

    (delete)api document

--- accounts/views.py
+++ accounts/views.py
@@ -24,15 +24,6 @@ class UserViewSet(mixins.RetrieveModelMixin,
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    @swagger_auto_schema(operation_summary="메인 페이지(유저 리스트)", operation_description='메인 페이지의 컨텐츠로 이용될 유저 리스트입니다.')
-    def list(self,request,*args, **kwargs):
-        return super().list(request,*args, **kwargs)
-
-    @swagger_auto_schema(operation_summary="개인 프로필 조회")
-    def retrieve(self, request, *args, **kwargs):
-        return super().retrieve(request, *args, **kwargs)
-
-    @swagger_auto_schema(operation_summary="개인 프로필 수정 및 회원가입 폼 저장", operation_description='1차적으로 회원가입은 서버에 유효한 토큰을 보냈을때 가입되며 이후 회원가입폼 정보를 입력하게되면 저장할때 쓰이는 api입니다. 또한 추후 자신의 프로필정보를 수정할때도 이용됩니다.')
     def update(self, request, *args, **kwargs):
         categories = request.data.get('categories', None)
         partial = kwargs.pop('partial', False)
@@ -52,7 +43,6 @@ class UserViewSet(mixins.RetrieveModelMixin,
                 instance.categories.add(category)
         return Response(serializer.data)
 
-    @swagger_auto_schema(operation_summary="닉네임 중복체크",operation_description="/api/users/check_nickname?nickname=~~와 같이 요청해주면 됩니당~")
     @action(methods=['get'], detail=False)
     def check_nickname(self, request):
         nickname = request.query_params.get('nickname')
@@ -68,7 +58,6 @@ class UserViewSet(mixins.RetrieveModelMixin,
             }
             return Response(res, status=status.HTTP_200_OK)
 
-    @swagger_auto_schema(operation_summary="카카오 소셜로그인",request_body=SocialLoginSerializer)
     @action(methods=['post'], detail=False)
     def kakao(self, request):
         data = {}
@@ -101,7 +90,6 @@ class UserViewSet(mixins.RetrieveModelMixin,
             data['social_type'] = user.social_type
             return Response(data, status=status.HTTP_201_CREATED)
 
-    @swagger_auto_schema(operation_summary="구글 소셜로그인", request_body=SocialLoginSerializer)
     @action(methods=['post'], detail=False)
     def google(self, request):
         data = {}
@@ -133,7 +121,6 @@ class UserViewSet(mixins.RetrieveModelMixin,
             data['social_type'] = user.social_type
             return Response(data, status=status.HTTP_201_CREATED)
 
-    @swagger_auto_schema(operation_summary="유저 팔로우", operation_description='헤더의 토큰값을 필수로 넣어주세요')
     @action(methods=['post'], detail=True)
     def follow(self, request, pk):
         follow = FollowRelation.objects.filter(follower=request.user, following_id=pk).first()
--- config/urls.py
+++ config/urls.py
@@ -13,30 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls.static import static
 from django.contrib import admin
-
-from django.urls import path, include, re_path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
-
+from django.urls import path, include
 from accounts.views import ping
-from config.settings import base
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="DreamIn API",
-        default_version='v1',
-        description=
-        """
-        DreamIn API 문서 페이지입니다.
-        """,
-    ),
-    validators=['flex'],
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
 
 
 urlpatterns = [
@@ -46,12 +25,4 @@ urlpatterns = [
     path('api/', include('teams.urls')),
     path('api/', include('posts.urls')),
     path('', ping, name='root_url_confirm'),
-]
-
-
-
-urlpatterns += [
-   re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   re_path(r'^swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   re_path(r'^redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
